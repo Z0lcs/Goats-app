a87a5d20eca4e7c7b1c5f7e8b7a1d664f0b91db6
Zolcs <aranyzoli11@gmail.com>
2026-09-16 16:10:13 +0200

    Egyből fog a pwa frissülni elv

--- sw.js
+++ sw.js
@@ -1,7 +1,16 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker telepítve');
+self.addEventListener('install', (event) => {
+    // Azonnal aktiváljuk az új Service Workert, ne várakozzon
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+self.addEventListener('activate', (event) => {
+    // Átveszi az irányítást az összes nyitott ablak felett
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    // Hálózati kérés frissítéssel (bypass browser HTTP cache)
+    event.respondWith(
+        fetch(event.request, { cache: 'no-cache' }).catch(() => caches.match(event.request))
+    );
 });
